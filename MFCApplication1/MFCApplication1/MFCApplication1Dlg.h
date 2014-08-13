b2cba9ddb659a799d4eb332e099df67762cfd928
edward <jongiree@gmail.com>
2014-08-13 15:06:22 -0700

    First Commit

---
+++ MFCApplication1/MFCApplication1/MFCApplication1Dlg.h
@@ -0,0 +1,44 @@
+
+// MFCApplication1Dlg.h : header file
+//
+#include <string>
+using namespace std;
+
+#pragma once
+
+
+// CMFCApplication1Dlg dialog
+class CMFCApplication1Dlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCApplication1Dlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_MFCAPPLICATION1_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+	string Reverse(string str);
+	void Ascending(int *array, int nCount);
+
+	void revert_str(char *start, char* end);
+	bool is_letter(char l);
+	void revert_sentence(char *str);
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton4();
+};
